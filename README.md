0b677c9f4417856aff10a4ed333603365ea590d8
Fernando A. Damião <me@fadamiao.com>
2013-08-29 13:52:52 -0300

    Choose the BSD 3-Clause License for killer.bash Change README.md to standard Markdown Change tabs to spaces in killer.bash

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-killer.bash
-===========
+# killer.bash
 
-Script to do the function of pkill in Mac OS X 10.6 and 10.7 .
+Script to do the function of pkill in Mac OS X 10.6 and 10.7.
 
-## Specs
+
+## Infos
 * Made in OS : Mac OS X 10.6 (a.k.a Snow Leopard)
 * Tested in OS : Mac OS X 10.6, Mac OS X 10.7, Ubuntu 10.04 LTS, Ubuntu 12.04 LTS
 * Language Used : Shell Script (Bash)
 * Dependencies : ps, grep, awk, kill
+* License : BSD 3-Clause License
 
 
 ## Motivation
@@ -22,16 +23,14 @@ Please run first verify.bash to see the compatibility of your system with killer
 
 ### Using the killer.bash
 Put the killer.bash on the /opt or on the folder you choose.
-Edit your ~/.bash_profile or if doesn't exists just create it.
+Edit your `~/.bash_profile` or if doesn't exists just create it.
 
 #### Editing ~/.bash_profile
-Just put the bottom line in your ~/.bash_profile and killer.bash is ready for use.
-```
-source /opt/killer.bash
-```
+Just put the bottom line in your `~/.bash_profile` and killer.bash is ready for use.
+
+    source /opt/killer.bash
 
 #### Now how to use
 killer.bash it's simple to use. Just say the name of the process and be happy.
-```
-$ pkill TextEdit
-```
+
+    $ pkill TextEdit
--- killer.bash
+++ killer.bash
@@ -1,29 +1,35 @@
 #!/bin/bash
+# killer.bash - Script to do the function of pkill in Mac OS X 10.6 and 10.7.
+# Author: Fernando A. Damião <me@fadamiao.com>
+# Created At: 2012-10-24 18:24
+# Last Update: 2013-08-29 13:27
+# License: BSD 3-Clause License
+
 function pkill()
 {
-	if [ $# -ne 1 ]; then
-		echo "Syntax: pkill <ProcessName>"
-		return 1
-	fi
+    if [ $# -ne 1 ]; then
+        echo "Syntax: pkill <ProcessName>"
+        return 1
+    fi
 
-	if [ -z $1 ]; then
-		echo "Syntax: pkill <ProcessName>"
-		return 1
-	fi
+    if [ -z $1 ]; then
+        echo "Syntax: pkill <ProcessName>"
+        return 1
+    fi
 
-	PROCESSID=$(ps aux | grep "\<$1\>" | grep -v "grep" | grep -v "bash" | awk '{ print $2 }')
+    PROCESSID=$(ps aux | grep "\<$1\>" | grep -v "grep" | grep -v "bash" | awk '{ print $2 }')
 
-	if [ -z "$PROCESSID" ]; then
-		echo "Anyone to Kill"
-		return 1
-	fi
+    if [ -z "${PROCESSID}" ]; then
+        echo "Anyone to Kill"
+        return 1
+    fi
 
-	kill -9 "$PROCESSID"
-	if [ $? == 0 ]; then
-		echo "Kill Confirmed"
-		return 0
-	else
-		echo "Kill Denied"
-		return 1
-	fi
+    kill -9 "${PROCESSID}"
+    if [ $? == 0 ]; then
+        echo "Kill Confirmed"
+        return 0
+    else
+        echo "Kill Denied"
+        return 1
+    fi
 }
